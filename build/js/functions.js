cd87055e4854055d10a0d08c6f9f8ed00a1d6594
mmazitov <mazitov.nikolya@gmail.com>
2023-02-17 13:42:53 +0200

    part of review

--- build/js/functions.js
+++ build/js/functions.js
@@ -32,3 +32,28 @@ function initHeader() {
 		}
 	});
 }
+function initTabs() {
+	'use strict';
+	var tabsHolder = $('.products-content'),
+		tabsTitle = tabsHolder.find('.products-list .products-item'),
+		tabsContent = tabsHolder.find('.products-hodler .products-tab');
+	console.log(tabsTitle);
+	tabsTitle.each(function (index) {
+		$(this).attr('data-tab', index);
+		$('[data-tab="0"]').addClass('active');
+	});
+	tabsContent.each(function (index) {
+		$(this).attr('data-content', index);
+		$('[data-content="0"]').addClass('active');
+	});
+	$('[data-tab]').on({
+		click: function (e) {
+			$(this).addClass('active').siblings().removeClass('active');
+			$('[data-content=' + $(this).data('tab') + ']')
+				.addClass('active')
+				.siblings('[data-content]')
+				.removeClass('active');
+			e.preventDefault();
+		},
+	});
+}
